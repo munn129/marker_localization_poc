715fd635af4e9e809f39d50990089a3ac9365a23
moon_home <ans92175@gmail.com>
2024-01-27 01:54:21 +0900

    change camera parameter

--- marker_pose_estimation.py
+++ marker_pose_estimation.py
@@ -2,8 +2,14 @@ import cv2
 import numpy as np
 
 #camera_matrix = [1,0,0,0,1,0,0,0,1] # f_x, 0, c_x, 0, f_y, c_y, 0,0,1
-camera_matrix = np.array([1,0,2016,0,1,1512,0,0,1], dtype=np.float32).reshape(3,3)
-distort_coefficient = np.array([0,0,0,0], dtype=np.float32)
+#camera_matrix = np.array([1,0,2016,0,1,1512,0,0,1], dtype=np.float32).reshape(3,3)
+#distort_coefficient = np.array([0,0,0,0], dtype=np.float32)
+
+# iphone camera intrinsic parameter
+camera_matrix = np.array([[1.97547873e+03, 0.00000000e+00, 1.06077279e+03],
+                          [0.00000000e+00, 2.05341424e+03, 5.13500761e+02],
+                          [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]], dtype=np.float32)
+distort_coefficient = np.array([4.189866e-01, -3.496832e+00, -1.239132e-02, -1.649367e-03, 1.055315e+01], dtype=np.float32)
 
 marker_length = 0.03 #3cm?
 
