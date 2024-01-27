b3f50fe0e024289b86868bd33d62b2a9b1743fab
GiYoung Mun <ans92175@gmail.com>
2024-01-27 20:47:43 +0900

    code optimization

--- marker_pose_estimation_video.py
+++ marker_pose_estimation_video.py
@@ -27,22 +27,19 @@ if cap.isOpened():
     while True:
         ret, img = cap.read()
         if ret:
-            output_img = img[:]
             corners, ids, rejected = detector.detectMarkers(img)
-            if ids is not None:
-                rotation_vector = []
-                translation_vector = []
-
-                for i in range(0, len(ids)):
-                    _, rvec, tvec = cv2.solvePnP(object_points, corners[i], camera_matrix, distort_coefficient)
-                    rotation_vector.append(rvec)
-                    translation_vector.append(tvec)
-                    output_img = cv2.drawFrameAxes(img, camera_matrix, distort_coefficient, rotation_vector[i], translation_vector[i], 0.03)
-
-            cv2.imshow(video, output_img)
-            cv2.waitKey(33)
         else:
+            print('reading video has a problem')
             break
+
+        if ids is not None:
+            for i in range(0, len(ids)):
+                _, rvec, tvec = cv2.solvePnP(object_points, corners[i], camera_matrix, distort_coefficient)
+                img = cv2.drawFrameAxes(img, camera_matrix, distort_coefficient, rvec, tvec, 0.03)
+
+        cv2.imshow(video, img)
+        cv2.waitKey(33)
+
 else:
     print("cannot open video file")
 
