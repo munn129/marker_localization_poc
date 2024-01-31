457d7b1e3225867ff6021c69b8824174b5e981b9
GiYoung Mun <ans92175@gmail.com>
2024-01-31 12:57:58 +0900

    add homogeneous matrix function in marker_coordinate.py

--- marker_coordinate.py
+++ marker_coordinate.py
@@ -1,3 +1,6 @@
+import math
+import numpy as np
+
 marker_coordinate ={
     1 : (1,1),
     2 : (1.06, 1),
@@ -13,6 +16,11 @@ marker_coordinate ={
 
 def homogeneous_matrix_maker(roll, pitch, yaw, x, y, z): 
 
-    
-    
-    return roll
+    homogeneous_matrix = np.eye(4)
+    yaw_mat = np.array([math.cos(yaw), -math.sin(yaw), 0, math.sin(yaw), math.cos(yaw), 0, 0, 0, 1]).reshape(3,3)
+    pitch_mat = np.array([math.cos(pitch), 0, math.sin(pitch), 0, 1, 0, -math.sin(pitch), 0, math.cos(pitch)]).reshape(3,3)
+    roll_mat = np.array([1,0,0,0,math.cos(roll), -math.cos(roll), 0, math.sin(roll), math.cos(roll)]).reshape(3,3)
+    homogeneous_matrix[:3, :3] = yaw_mat @ pitch_mat @ roll_mat
+    homogeneous_matrix[:3, 3] = np.array([x,y,z]).reshape(1,3)
+
+    return homogeneous_matrix
